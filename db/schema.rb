0df453f98fd8ac5ca6c561356c3e7bb8bb3950b8
zdravko <tudumzagit@gmail.com>
2020-09-16 17:28:29 +0200

    Added connection model and fixed devise registration layouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_170221) do
+ActiveRecord::Schema.define(version: 2020_09_16_134643) do
+
+  create_table "connections", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_connections_on_friend_id"
+    t.index ["user_id"], name: "index_connections_on_user_id"
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
